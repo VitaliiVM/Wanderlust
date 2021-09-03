9e7289f13935e2a88f4a0a967189a39fc7923333
Vitalii <v.mirniy@icloud.com>
2021-09-03 12:20:41 +0300

    helper.js

--- files/public/helpers.js
+++ files/public/helpers.js
@@ -9,13 +9,13 @@ const createVenueHTML = (name, location, iconSource) => {
 
 const createWeatherHTML = (currentDay) => {
     console.log(currentDay.sys.country);
-    const usGb = `<h2>${weekDays[(new Date()).getDay()]}</h2>
+    const uSGb = `<h2>${weekDays[(new Date()).getDay()]}</h2>
 		   <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
 		<h2>Condition: ${currentDay.weather[0].description}</h2>
   	<img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
 
     if (currentDay.sys.country === 'US' || 'GB') {
-        return usGb;
+        return uSGb;
     }
     return `<h2>${weekDays[(new Date()).getDay()]}</h2>
 		<h2>Temperature: ${kelvinToCelcius(currentDay.main.temp)}&deg;C</h2>
